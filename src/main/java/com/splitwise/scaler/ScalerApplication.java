cc80d18a8a6cbce14d61a3176d00c87115fd8486
codekamit <kamitstar@icloud.com>
2023-12-13 09:40:07 +0530

    cleared more unwanted files

--- src/main/java/com/splitwise/scaler/ScalerApplication.java
+++ src/main/java/com/splitwise/scaler/ScalerApplication.java
@@ -1,8 +1,6 @@
 package com.splitwise.scaler;
 
 import com.splitwise.scaler.DTOs.RegisterUserRequestDTO;
-import com.splitwise.scaler.controllers.GroupController;
-import com.splitwise.scaler.controllers.UserController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,26 +12,8 @@ import java.util.Scanner;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class ScalerApplication implements CommandLineRunner {
-
-	@Autowired
-	private GroupController groupController;
-
-	@Autowired
-	private UserController userController;
-
+public class ScalerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ScalerApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Scanner scanner = new Scanner(System.in);
-		String firstName = scanner.nextLine();
-		String lastName = scanner.nextLine();
-		String email = scanner.nextLine();
-		String password = scanner.nextLine();
-		RegisterUserRequestDTO registerUserRequestDTO = new RegisterUserRequestDTO(firstName, lastName, email, password);
-		userController.registerUser(registerUserRequestDTO);
 	}
 }
